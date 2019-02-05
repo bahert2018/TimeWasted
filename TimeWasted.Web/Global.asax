41685c07f035bb2acaa14abf07dfc24b625e09a0
Baher Tawedrous <bahert2018@gmail.com>
2019-02-05 11:28:29 -0500

    Add project files.

---
+++ TimeWasted.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TimeWasted.Web.MvcApplication" Language="C#" %>
---
+++ TimeWasted.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TimeWasted.Web.Startup))]
+namespace TimeWasted.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
